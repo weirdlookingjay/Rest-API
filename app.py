d220fe598e5a150dc1dfc187513690a42a8c7844
Joe Clark <weirdlookingjay@live.com>
2024-06-05 18:48:32 -0400

    create an item in a store

--- app.py
+++ app.py
@@ -24,3 +24,16 @@ def create_store():
     new_store = {"name": request_data["name"], "items": []}
     stores.append(new_store)
     return new_store, 201
+
+@app.post("/store/<string:name>/item")
+def create_item(name):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {
+                "name": request_data["name"],
+                "price": request_data["price"]
+            }
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
